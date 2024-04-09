3d0b66233064278370d4f37e7b7e3256eff8032b
Jules White <juleswhite@127.0.0.1>
2024-04-09 21:50:43 +0100

    Update intial config options

--- src/client/Config.cs
+++ src/client/Config.cs
@@ -1,7 +1,7 @@
 namespace Saveshare;
 
 public sealed class Config {
-    public string ServerAddr { get; set; } = "http://YOUR-SERVER:8080";
+    public string ServerAddr { get; set; } = "http://your-server-here:8080";
     public bool DiscordLinked { get; set; } = false;
     public List<string> WatchedWorlds { get; set; } = new();
 }
